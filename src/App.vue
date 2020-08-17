d9b6258f0a24c299a35f56aa0ecb279272ebfe2b
unknown <marcos.alois.oliva@gmail.com>
2020-08-16 18:12:17 -0700

    Initial cognito basich auth

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <amplify-authenticator >
+     <amplify-sign-in header-text="MIIA Sign In" slot="sign-in"></amplify-sign-in>
+
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="amplify-signout">
+       <amplify-sign-out></amplify-sign-out>
+    </div>
+
+    </amplify-authenticator>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue"
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#amplify-signout {
+  width: 100px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import '@aws-amplify/ui-vue';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
