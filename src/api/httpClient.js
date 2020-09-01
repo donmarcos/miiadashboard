3340b40f616e6263efb6a1191cbbb143c601ea1f
unknown <marcos.alois.oliva@gmail.com>
2020-09-01 10:58:00 -0700

    Adding a domain for ML URL

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
+const https = require('https');
+
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
 
 const httpClient = axios.create({
     baseURL: "https://qd480p28u9.execute-api.us-west-2.amazonaws.com/dev/miiaapi",
     timeout: 5000, // indicates, 1000ms ie. 1 second
     headers: {
         "Content-Type": "application/json",
-    }
+    },
+    httpsAgent: agent
 });
 
 export {
--- src/api/ml.api.js
+++ src/api/ml.api.js
@@ -1,16 +1,18 @@
-import { httpClient } from "./httpClient"
+import {
+    httpClient
+} from "./httpClient"
 
 const BMI_END_POINT = "/predictBMI"
 const EMO_END_POINT = "/predictemotion"
 
 // set the new base URL
 var config = {
-  baseURL: "https://54.179.135.246:5000",
-  timeout: 15000, // indicates, 1000ms ie. 1 second
-  headers: {
-    "Content-Type": "multipart/form-data",
-  },
-  responseType: "text",
+    baseURL: "https://miiaml.tk:5000/",
+    timeout: 15000, // indicates, 1000ms ie. 1 second
+    headers: {
+        "Content-Type": "multipart/form-data",
+    },
+    responseType: "text",
 }
 
 //httpClient.defaults.baseURL = 'http://54.179.135.246:5000';
@@ -18,22 +20,25 @@ var config = {
 //httpClient.headers = "Content-Type": "multipart/form-data" ;
 // const getTypes = () => httpClient.get(END_POINT);
 
-const getBMI = function(FILE) {
-  console.log("getBMI axios call")
-  console.log(FILE)
-  const formData = new FormData()
-  formData.append("file", FILE, FILE.name)
-  formData.append("name", FILE.name)
-  return httpClient.post(BMI_END_POINT, formData, config)
+const getBMI = function (FILE) {
+    console.log("getBMI axios call")
+    console.log(FILE)
+    const formData = new FormData()
+    formData.append("file", FILE, FILE.name)
+    formData.append("name", FILE.name)
+    return httpClient.post(BMI_END_POINT, formData, config)
 }
 
-const getEmotion = function(FILE) {
-  console.log("getEmotion axios call")
-  console.log(FILE)
-  const formData = new FormData()
-  formData.append("file", FILE, FILE.name)
-  formData.append("name", FILE.name)
-  return httpClient.post(EMO_END_POINT, formData, config)
+const getEmotion = function (FILE) {
+    console.log("getEmotion axios call")
+    console.log(FILE)
+    const formData = new FormData()
+    formData.append("file", FILE, FILE.name)
+    formData.append("name", FILE.name)
+    return httpClient.post(EMO_END_POINT, formData, config)
 }
 
-export { getBMI, getEmotion }
+export {
+    getBMI,
+    getEmotion
+}
